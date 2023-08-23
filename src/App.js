b3b7bc3dc58abd6a2ef0fb5b4177271ac0c2f78a
ZAFIR123456 <102862385+ZAFIR123456@users.noreply.github.com>
2023-08-23 23:20:30 +0500

    footer completed

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import Banner from './components/Banner'
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
+import Footer from './components/Footer';
 const App = () => {
   return (
     <div>
-      <Header/>
-      <Banner/>
-      <Skills/>
-      <Projects/>
-      <Contact/>
+      <Header />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   )
 }
